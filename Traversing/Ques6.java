285d7c8f43bddb7d4137821160ceff6638f38449
Gaurav kumar <gauravkumar13045@gmail.com>
2025-11-26 23:52:25 +0530

    Ques6 completed

--- Traversing/Ques6.java
+++ Traversing/Ques6.java
@@ -2,6 +2,15 @@
 
 package Traversing;
 
+import java.util.Arrays;  
 public class Ques6 {
-    
+    public static void main(String[] args){
+        int[] arr = { 5, 10, 15, 20, 25 };
+
+        for(int u = arr.length - 1; u >= 0; u--){
+            System.out.print(arr[u] + " ");
+        }
+
+        System.out.println("\nOriginal Array: " + Arrays.toString(arr));
+    }
 }
