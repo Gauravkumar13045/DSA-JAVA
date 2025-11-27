d26699b3509d1df343528bfb602178883db71c68
Gaurav kumar <gauravkumar13045@gmail.com>
2025-11-27 18:24:18 +0530

    Ques9 completed

---
+++ Insert/Ques9.java
@@ -0,0 +1,21 @@
+// Insert element at end
+package Insert;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class Ques9 {
+    public static void main(String[] args) {
+        int[] arr = { 5, 10, 15, 20, 25};
+        int[] newArr = new int[arr.length + 1];
+        int value = 630;
+
+        for(int i = 0; i < arr.length; i++){
+            newArr[i] = arr[i];
+            newArr[arr.length] = value;
+
+        }
+               System.out.println("new Array : " + Arrays.toString(newArr));
+
+    }
+}
