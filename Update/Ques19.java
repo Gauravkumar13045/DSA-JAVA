34574176c851121aa46e23cf721d9abf832fa0ef
Gaurav kumar <gauravkumar13045@gmail.com>
2025-11-29 23:00:02 +0530

    Ques19 completed

---
+++ Update/Ques19.java
@@ -0,0 +1,20 @@
+// If value exists → increase it by 10%
+package Update;
+
+import java.util.Arrays;
+
+public class Ques19 {
+    public static void main(String[] args) {
+        double[] arr = { 5, 100, 20, 100, 25, 100, 19 };
+        double oldValue = 100;
+        double increase = 0.1; // 10%
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == oldValue) {
+                arr[i] = arr[i] + arr[i] * increase; // increase by 10%
+            }
+        }
+
+        System.out.println("After Updation: " + Arrays.toString(arr));
+    }
+}
